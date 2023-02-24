1e2f872f165cf2c722b128125af418ff7a25bd55
马凯 <v-makai@sinochem.com>
2023-02-24 15:18:17 +0800

    feat: 项目初始化

---
+++ src/shims-tsx.d.ts
@@ -0,0 +1,27 @@
+// import Vue, { VNode } from 'vue'
+
+// declare global {
+//   namespace JSX {
+//     interface Element extends VNode {}
+//     interface ElementClass extends Vue {}
+//     interface IntrinsicElements {
+//       [elem: string]: any
+//     }
+//   }
+// }
+
+import Vue, { VNode } from 'vue'
+import { ComponentRenderProxy } from '@vue/composition-api'
+
+declare global {
+  namespace JSX {
+    interface Element extends VNode {}
+    interface ElementClass extends ComponentRenderProxy {}
+    interface ElementAttributesProperty {
+      $props: any // specify the property name to use
+    }
+    interface IntrinsicElements {
+      [elem: string]: any
+    }
+  }
+}
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const ScriptSetup = require('unplugin-vue2-script-setup/webpack').default
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  parallel: false, // disable thread-loader, which is not compactible with this plugin
+  configureWebpack: {
+    plugins: [
+      ScriptSetup({
+        /* options */
+      })
+    ]
+  },
+  chainWebpack(config) {
+    // disable type check and let `vue-tsc` handles it
+    config.plugins.delete('fork-ts-checker')
+  }
+})
